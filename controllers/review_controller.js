6129fc4e064fc1cb7b16053bc468b684fecdb660
Nir Miranda <nirmiranda2@gmail.com>
2024-01-09 17:59:04 +0200

    finished movie and review

--- controllers/review_controller.js
+++ controllers/review_controller.js
@@ -1,22 +1,42 @@
-const ReviewModel = require("../Models/reviewsModel");
-const User = require("../Models/userModel")
-const Movie = require("../Models/movieModel")
+const Review = require("../models/review_model");
+const User = require("../models/user_model")
+const Movie = require("../models/movie_model")
 
-module.exports.getReview = async (req, res) => {
-    const Review = await ReviewModel.find();
-    res.send(Review);
+const getReviews = async (req, res) => {
+    try {
+        let reviews = '';
+        if (req.query._id) {
+            reviews = await Review.find({ _id: req.query._id })
+        }
+        else {
+            reviews = await Review.find();
+        }
+        res.send(reviews);
+    } catch (error) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+const getReviewById = async (req, res) => {
+    try {
+        const review = await Review.findById(req.params._id)
+        res.send(review);
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 }
 
 //add Review
-module.exports.addReview = async (req, res) => {
-    const { name, date, text, starRank, movieId, userId } = req.body
+const addReviews = async (req, res) => {
+    const { date,reviewerId,movieId, rating, image, text } = req.body
     try {
         const movie = await Movie.findById(movieId);
-        const user = await User.findById(userId);
-        const newReview = new ReviewModel({ name, date, text, starRank, movieId, userId });
+        const user = await User.findById(reviewerId);
+        const newReview = new Review({ date,reviewerId,movieId, rating, image, text });
         const savedReview = await newReview.save();
-
         movie.reviews.push(savedReview._id);
+        // user.reviews.push(savedReview._id);
+
         const changedMovie = await Movie.findByIdAndUpdate(movie._id, movie);
         res.send(savedReview);
     } catch (err) {
@@ -24,27 +44,35 @@ module.exports.addReview = async (req, res) => {
     }
 }
 
-module.exports.updateReview = async (req, res) => {
-    const { _id, name, date, text, starRank } = req.body;
-    const newReview = await ReviewModel.findByIdAndUpdate(_id, { name, date, text, starRank });
+const updateReview = async (req, res) => {
+    const { _id, date,reviewerId,movieId, rating, image, text } = req.body;
+    const newReview = await Review.findByIdAndUpdate(_id, { date,reviewerId,movieId, rating, image, text } , { new: true });
     res.send(newReview);
     console.log("update succeded");
 }
 
-module.exports.deleteReview = async (req, res) => {
-    const { _id } = req.body;
-    const deletedReview = await ReviewModel.findById(_id);
-    const movie = await Movie.findById(deletedReview.movieId);
-    console.log(movie);
-    movie.reviews.pull(deletedReview._id);
-    await ReviewModel.findByIdAndDelete(deletedReview._id);
-    const changedMovie = await Movie.findByIdAndUpdate(movie._id, movie);
-    res.send("Review deleted")
+const deleteReview = async (req, res) => {
+    const { _id } = req.params;
+    const deletedReview = await Review.findById(_id);
+    if (deletedReview) {
+        const movie = await Movie.findById(deletedReview.movieId);
+        movie.reviews.pull(_id);
+        await Review.findByIdAndDelete(_id);
+        const changedMovie = await Movie.findByIdAndUpdate(movie._id, movie);
+        res.send("Review deleted")
+        console.log("Review deleted");
+
+    }
+    else {
+        res.send("Review not found");
+        console.log("Review not found");
+    }
 }
+
 module.exports = {
-    getAllMovies,
-    postMovie,
-    getMovieByName,
-    putMovieByName,
-    deleteMovieByName,
+    getReviews,
+    getReviewById,
+    addReviews,
+    updateReview,
+    deleteReview,
 };
