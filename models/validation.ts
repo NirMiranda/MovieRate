bb0ed8ba48b7cf999a706632428b030afb52326a
Dorins12 <27dorin12@gmail.com>
2024-01-17 14:19:02 +0200

    change the register that it work in the frontend more good.

--- models/validation.ts
+++ models/validation.ts
@@ -1,10 +1,10 @@
 import Joi from '@hapi/joi';
 
 const authSchema = Joi.object({
-    name: Joi.string(),
+    name: Joi.string().required(),
     email: Joi.string().email().lowercase().required(),
     password: Joi.string().min(6).max(14).required(),
-    age: Joi.number().min(6).max(120),
+    age: Joi.number().min(6).max(120).required(),
 });
 
 export default authSchema;
