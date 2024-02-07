6c8c2c915b06b39b32627a9455d787f372683b11
Nir Miranda <nirmiranda2@gmail.com>
2024-02-07 21:14:02 +0200

    ..

--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import { Application } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import swaggerJsDoc from 'swagger-jsdoc';
 import http from 'http';
+import fs from 'fs';
+import https from 'https';
 import { Server } from 'socket.io'; // Import Server from socket.io
 import { handleClient } from './socket';
 
@@ -25,9 +27,12 @@ const startServer = async () => {
         const specs = swaggerJsDoc(options);
         app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
-        const port = process.env.PORT || 3003;
-
-        const server = http.createServer(app);
+        const port = process.env.PORT;
+        const options2= {
+            key: fs.readFileSync('client-key.pem'),
+            cert: fs.readFileSync('client-cert.pem')
+        }
+        const server = https.createServer(options2, app);
         const io = new Server(server); // Initialize Socket.io server
         handleClient(io);
 
