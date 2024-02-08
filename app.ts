c02b5f96372a70fb4e74d5e5e255e28f1a781bc0
Nir Miranda <nirmiranda2@gmail.com>
2024-02-08 15:12:09 +0200

    enable cors

--- app.ts
+++ app.ts
@@ -27,7 +27,9 @@ const initApp = (): Promise<Application> => {
         // const HttpServer = http.createServer(app)
         // new ServerSocket(HttpServer);
 
-        app.use(cors());
+        app.use(cors({
+            origin: 'https://nirmiranda.github.io',
+          }));
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: true }));
 
